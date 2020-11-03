40dd40f07ed66610377ab3db9b4057f7f912aaca
Julian Nymark <j@juliannymark.com>
2020-11-03 21:56:26 +0100

    problem 2

--- src/euler.rs
+++ src/euler.rs
@@ -5,6 +5,12 @@ mod tests {
         let answer = super::problem_1(10);
         assert_eq!(answer, 23);
     }
+
+    #[test]
+    fn problem_2() {
+        let answer = super::problem_2(90);
+        assert_eq!(answer, 44);
+    }
 }
 
 pub fn problem_1(upper: i32) -> i32 {
@@ -13,6 +19,25 @@ pub fn problem_1(upper: i32) -> i32 {
         if i % 3 == 0 || i % 5 == 0 {
             sum += i;
         }
+    }
+    return sum;
+}
+
+///
+/// NOTE: there is a functional way to do this too!
+///
+pub fn problem_2(upper: i32) -> i32 {
+    let mut sum = 0;
+    let mut previous = 1;
+    let mut current = 2;
+    while current < upper {
+        if current % 2 == 0 {
+            sum += current;
+        }
+        
+        let next = previous + current;
+        previous = current;
+        current = next;
     }
     return sum;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod euler;
 
 fn main() {
-    println!("problem 1: {}", euler::problem_1(1000));
+    println!("problem 1: {}", euler::problem_1(1_000));
+    println!("problem 2: {}", euler::problem_2(4_000_000));
 }
